d85c13c0f88a88d6719f894b7acd710ac9945ded
Markus Tacker <m@coderbyheart.com>
2023-08-26 14:14:36 +0200

    minimal example

---
+++ test.spec.js
@@ -0,0 +1,6 @@
+import { describe, test as it } from "node:test";
+import assert from "node:assert";
+
+describe("calculator", () => {
+  it("should sum up two numbers", () => assert.equal(1 + 2, 3));
+});
